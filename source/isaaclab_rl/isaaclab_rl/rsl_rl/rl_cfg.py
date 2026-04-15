21178911870ffae31a703daf568a4a87c744480b
davin-win-kyi <davinkyi@gmail.com>
2026-04-14 23:44:06 -0700

    removing share_cnn_encoders

--- source/isaaclab_rl/isaaclab_rl/rsl_rl/rl_cfg.py
+++ source/isaaclab_rl/isaaclab_rl/rsl_rl/rl_cfg.py
@@ -211,7 +211,7 @@ class RslRlPpoAlgorithmCfg:
     Otherwise, the advantage is normalized over the entire collected trajectories.
     """
 
-    share_cnn_encoders: bool = False
+    # share_cnn_encoders: bool = False
     """Whether to share the CNN networks between actor and critic, in case CNNModels are used. Defaults to False."""
 
     rnd_cfg: RslRlRndCfg | None = None
